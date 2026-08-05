92938a7b527e7f1df584646540b32f32b6cd4454
Behrouz Karimi <125777824+bkarimii@users.noreply.github.com>
2026-08-05 14:51:02 +0100

    MTDSA-35868 - Update access to APIs to be Controlled Access - Individuals Foreign Income API (#71)
    * MTDSA-35868
    
    * adding tests for control access switch
    
    * MTDSA-35868

--- test/api/definition/ApiDefinitionSpec.scala
+++ test/api/definition/ApiDefinitionSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import play.api.libs.json.{JsValue, Json}
 
 class ApiDefinitionSpec extends UnitSpec {
 
-  private val apiVersion: APIVersion       = APIVersion(Version3, APIStatus.ALPHA, endpointsEnabled = true)
+  private val apiVersion: APIVersion       = APIVersion(Version3, APIStatus.ALPHA, APIAccessType.PUBLIC, endpointsEnabled = true)
   private val apiDefinition: APIDefinition = APIDefinition("b", "c", "d", List("category"), List(apiVersion), Some(false))
   private val definition                   = Definition(apiDefinition)
 
@@ -30,6 +30,7 @@ class ApiDefinitionSpec extends UnitSpec {
        |{
        | "version": "3.0",
        |"status": "ALPHA",
+       |"access": "PUBLIC",
        |"endpointsEnabled": true
        |}
        |""".stripMargin)
--- test/definition/ForeignIncomeApiDefinitionFactorySpec.scala
+++ test/definition/ForeignIncomeApiDefinitionFactorySpec.scala
@@ -19,7 +19,7 @@ package definition
 import api.config.Deprecation.NotDeprecated
 import api.config.MockAppConfig
 import api.definition.APIStatus.BETA
-import api.definition.{APIDefinition, APIVersion, Definition}
+import api.definition.{APIAccessType, APIDefinition, APIVersion, Definition}
 import api.routing.Version2
 import api.utils.UnitSpec
 import cats.implicits.catsSyntaxValidatedId
@@ -34,6 +34,7 @@ class ForeignIncomeApiDefinitionFactorySpec extends UnitSpec with MockAppConfig 
         MockedAppConfig.apiStatus(Version2) returns "BETA"
         MockedAppConfig.endpointsEnabled(Version2).returns(true).anyNumberOfTimes()
         MockedAppConfig.deprecationFor(Version2).returns(NotDeprecated.valid).anyNumberOfTimes()
+        MockedAppConfig.controlledAccessEnabled returns false
 
         val apiDefinitionFactory = new ForeignIncomeApiDefinitionFactory(mockAppConfig)
 
@@ -48,12 +49,50 @@ class ForeignIncomeApiDefinitionFactorySpec extends UnitSpec with MockAppConfig 
                 APIVersion(
                   Version2,
                   status = BETA,
+                  access = APIAccessType.PUBLIC,
                   endpointsEnabled = true
                 )
               ),
               requiresTrust = None
             )
           )
+      }
+    }
+  }
+
+  "ForeignIncomeApiDefinitionFactory" when {
+
+    "the access level is set" when {
+      "the controlled access flag is enabled" should {
+        "return CONTROLLED" in {
+
+          MockedAppConfig.apiGatewayContext returns "individuals/foreign-income"
+          MockedAppConfig.endpointsEnabled(Version2)
+          MockedAppConfig.apiStatus(Version2) returns "BETA"
+          MockedAppConfig.deprecationFor(Version2).returns(NotDeprecated.valid).anyNumberOfTimes()
+
+          MockedAppConfig.controlledAccessEnabled returns true
+
+          val definition: Definition = new ForeignIncomeApiDefinitionFactory(mockAppConfig).definition
+
+          definition.api.versions.head.access shouldBe APIAccessType.CONTROLLED
+        }
+      }
+
+      "the controlled access flag is disabled" should {
+        "return PUBLIC" in {
+
+          MockedAppConfig.apiGatewayContext returns "individuals/foreign-income"
+          MockedAppConfig.endpointsEnabled(Version2)
+          MockedAppConfig.apiStatus(Version2) returns "BETA"
+          MockedAppConfig.deprecationFor(Version2).returns(NotDeprecated.valid).anyNumberOfTimes()
+
+          MockedAppConfig.controlledAccessEnabled returns false
+
+          val definition: Definition = new ForeignIncomeApiDefinitionFactory(mockAppConfig).definition
+
+          definition.api.versions.head.access shouldBe APIAccessType.PUBLIC
+        }
       }
     }
   }
